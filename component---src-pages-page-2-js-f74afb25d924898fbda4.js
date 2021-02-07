3b59945bec973b05346052a05d94a782f1507f6b
Kunal Raghav <kraghav123@gmail.com>
2021-02-07 17:57:26 +0530

    Updates

---
+++ component---src-pages-page-2-js-f74afb25d924898fbda4.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{p5nM:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),o=a.n(n),l=a("Wbzz"),c=a("8k0H"),r=a("vrFN");t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-f74afb25d924898fbda4.js.map
